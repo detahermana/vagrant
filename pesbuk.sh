c3009ac563814de883bf2397b5af20568ec620c5
Deta Hermana <detahermana@gmail.com>
2022-04-16 10:21:42 +0700

    update shell script pesbuk

--- pesbuk.sh
+++ pesbuk.sh
@@ -8,17 +8,27 @@ apt-get update -y
 apt-get upgrade -y
 apt-get install -y apache2 php php-mysql
 apt-get install -y mysql-server
+apt-get install -y unzip
 
-mysql -u root -e "create user $DBUSER@'localhost' identified by '$DBPASS'; grant all privileges on *.* to $DBUSER@'localhost'"
-mysql -u root -e "create database $DBNAME"
+#mysql -u root -e "create user $DBUSER@'localhost' identified by '$DBPASS'; grant all privileges on *.* to $DBUSER@'localhost'"
+#mysql -u root -e "create database $DBNAME"
+
+sudo mysql -u root <<MYSQL_SCRIPT
+CREATE USER '$DBUSER'@'localhost' IDENTIFIED by '$DBPASS';
+GRANT ALL PRIVILEGES ON *.* to '$DBUSER'@'localhost';
+FLUSH PRIVILEGES;
+MYSQL_SCRIPT
+
+sudo mysql -u$DBUSER -p$DBPASS<<MYSQL_SCRIPT
+CREATE DATABASE $DBNAME;
+MYSQL_SCRIPT
 
 cd
 #wget https://github.com/sdcilsy/sosial-media/archive/master.zip
 wget https://github.com/detahermana/pesbuk-samehada/archive/master.zip
-apt-get install -y unzip
 unzip master.zip
 sudo rm /var/www/html/index.html
-sudo mv pesbuk-samehada-master/* /var/www/html
+sudo mv pesbuk-samehada-main/* /var/www/html
 
 cd /var/www/html/
-sudo mysql -u root $DBNAME < dump.sql
+sudo mysql -u $DBUSER -p$DBPASS $DBNAME < dump.sql
